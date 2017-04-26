02904a26b91f3bf35f9a37e10684075b9fa7c0a2
SoulJa <soulja@jonnydeiMac.local>
2017-04-26 15:17:12 +0800

    初始化项目

---
+++ RealmDemo_OC/RealmDemo_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RealmDemo_OC
+//
+//  Created by SoulJa on 2017/4/26.
+//  Copyright © 2017年 shengpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RealmDemo_OC/RealmDemo_OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RealmDemo_OC
+//
+//  Created by SoulJa on 2017/4/26.
+//  Copyright © 2017年 shengpay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
